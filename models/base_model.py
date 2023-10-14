9b4dc867a20669ba7db63745ab9ec6fe43ee89de
GirumZ <girum.zebe@gmail.com>
2023-10-14 04:25:36 -0700

    added the file_storage file and updated base_model file

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -23,6 +24,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """This returns a string representation of an instance"""
@@ -30,10 +32,11 @@ class BaseModel:
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
                                      self.__dict__)
 
-    def save(self, updated_at):
+    def save(self):
         """Updates attribute updated_at to the current time"""
 
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns the dictionary of the represented instances"""
